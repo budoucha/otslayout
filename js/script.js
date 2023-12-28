9269a5fb03d0ebf0d8857159e07a46fca5eea9fb
budoucha <budoucha@gmail.com>
2023-12-29 01:27:22 +0900

    背面パネルが邪魔になるのを防止

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 const rotBtnL = document.querySelector('.guide#guideL');
 const rotBtnR = document.querySelector('.guide#guideR');
 
+/* 背面パネルを非表示にする(正面パネルの邪魔にならないようにする) */
+const hideIfBack = (element) => {
+    const transform = element.computedStyleMap().get('transform');
+    const rotate = Array.from(transform).find(transform => transform instanceof CSSRotate);
+    const angle = rotate ? +rotate.angle.value : 0;
+
+    if (angle === 180) {
+        element.classList.add("hidden");
+    }
+    else {
+        element.classList.remove("hidden");
+    }
+}
+
+const panes = document.querySelectorAll('.pane');
+panes.forEach((pane) => {
+    hideIfBack(pane);
+})
+
+
 rotBtnL.addEventListener('click', () => {
-    const panes = document.querySelectorAll('.pane');
     panes.forEach((pane) => {
         const transform = pane.computedStyleMap().get('transform');
         const rotate = Array.from(transform).find(transform => transform instanceof CSSRotate);
@@ -11,11 +30,11 @@ rotBtnL.addEventListener('click', () => {
         const translateValue = translate ? `translateZ(${translate.z.value}${translate.z.unit})` : '';
         const newAngle = (Math.round(currentAngle / 90) - 1) * 90; //連打でずれていくのを防ぐため離散化する
         pane.style.transform = `rotateY(${newAngle}deg) ${translateValue}`;
+        hideIfBack(pane);
     });
 })
 
 rotBtnR.addEventListener('click', () => {
-    const panes = document.querySelectorAll('.pane');
     panes.forEach((pane) => {
         const transform = pane.computedStyleMap().get('transform');
         const rotate = Array.from(transform).find(transform => transform instanceof CSSRotate);
@@ -24,6 +43,7 @@ rotBtnR.addEventListener('click', () => {
         const translateValue = translate ? `translateZ(${translate.z.value}${translate.z.unit})` : '';
         const newAngle = (Math.round(currentAngle / 90) + 1) * 90;
         pane.style.transform = `rotateY(${newAngle}deg) ${translateValue}`;
+        hideIfBack(pane);
     });
 })
 
