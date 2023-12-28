7e5a9b7f4e5912c75e43ce2ce4372bc017774085
budoucha <budoucha@gmail.com>
2023-12-29 07:19:07 +0900

    ちょっと見通しをよくした

--- js/script.js
+++ js/script.js
@@ -1,54 +1,46 @@
 const rotBtnL = document.querySelector('.guide#guideL');
 const rotBtnR = document.querySelector('.guide#guideR');
 
-/* 背面パネルを非表示にする(正面パネルの邪魔にならないようにする) */
-const panes = document.querySelectorAll('.pane');
-panes.forEach((pane) => {
+const getAngleAndTranslate = (pane) => {
     const transform = pane.computedStyleMap().get('transform');
     const rotate = Array.from(transform).find(transform => transform instanceof CSSRotate);
-    const currentAngle = rotate ? +rotate.angle.value : 0;
-    if (currentAngle % 360 === 180) {
+    const translate = Array.from(transform).find(transform => transform instanceof CSSTranslate);
+    const angleValue = rotate ? +rotate.angle.value : 0;
+    const translateValue = translate ? `translateZ(${translate.z.value}${translate.z.unit})` : '';
+    return { angleValue, translateValue };
+}
+
+const movePane = (pane, angle, translateValue) => {
+    pane.style.transform = `rotateY(${angle}deg) ${translateValue}`;
+    if (angle % 360 === 180) {
         pane.classList.add("back");
     }
     else {
         pane.classList.remove("back");
     }
+}
+
+const panes = document.querySelectorAll('.pane');
+
+/* 背面パネルを非表示にする(正面パネルの邪魔にならないようにする) */
+panes.forEach((pane) => {
+    const { angleValue, translateValue} = getAngleAndTranslate(pane);
+    movePane(pane, angleValue, translateValue);
 })
 
 rotBtnL.addEventListener('click', () => {
     panes.forEach((pane) => {
-        const transform = pane.computedStyleMap().get('transform');
-        const rotate = Array.from(transform).find(transform => transform instanceof CSSRotate);
-        const translate = Array.from(transform).find(transform => transform instanceof CSSTranslate);
-        const currentAngle = rotate ? +rotate.angle.value : 0;
-        const translateValue = translate ? `translateZ(${translate.z.value}${translate.z.unit})` : '';
-        const newAngle = ((Math.round(currentAngle / 90) - 1) * 90); //連打でずれていくのを防ぐため離散化する
-        // console.log(newAngle, newAngle % 360)
-        pane.style.transform = `rotateY(${newAngle}deg) ${translateValue}`;
-        if (newAngle % 360 === 180) {
-            pane.classList.add("back");
-        }
-        else {
-            pane.classList.remove("back");
-        }
+        const { angleValue, translateValue } = getAngleAndTranslate(pane);
+        const newAngle = ((Math.round(angleValue / 90) - 1) * 90); //連打でずれていくのを防ぐため離散化する
+        movePane(pane, newAngle, translateValue);
     });
 })
 
 rotBtnR.addEventListener('click', () => {
     panes.forEach((pane) => {
-        const transform = pane.computedStyleMap().get('transform');
-        const rotate = Array.from(transform).find(transform => transform instanceof CSSRotate);
-        const translate = Array.from(transform).find(transform => transform instanceof CSSTranslate);
-        const currentAngle = rotate ? +rotate.angle.value : 0;
-        const translateValue = translate ? `translateZ(${translate.z.value}${translate.z.unit})` : '';
-        const newAngle = ((Math.round(currentAngle / 90) + 1) * 90);
-        pane.style.transform = `rotateY(${newAngle}deg) ${translateValue}`;
-        if (newAngle % 360 === 180) {
-            pane.classList.add("back");
-        }
-        else {
-            pane.classList.remove("back");
-        }
+        const { angleValue, translateValue } = getAngleAndTranslate(pane);
+        const newAngle = ((Math.round(angleValue / 90) + 1) * 90);
+        movePane(pane, newAngle, translateValue);
     });
 })
 
